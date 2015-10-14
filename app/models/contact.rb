354b1b9245e8080da8026972edb17fdd3218cf99
zayrt <zelloufi.ilyas@gmail.com>
2015-10-14 22:40:34 +0200

    best validation

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,9 +1,9 @@
 class Contact < ActiveRecord::Base
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
 	VALID_USERNAME_REGEX = /\A[a-zA-Z]{1}[a-z0-9]+\z/
-	validates :firstname,	presence: true, length: { in: 3..30 }, format: { with: VALID_USERNAME_REGEX }
+	validates :firstname,	presence: true, length: { in: 3..30 }, format: { with: VALID_USERNAME_REGEX },
+						uniqueness: {scope: :lastname, case_sensitive: false, message: "and Lastname have already been taken"}
 	validates :lastname,	presence: true, length: { in: 3..30 }, format: { with: VALID_USERNAME_REGEX }
 	validates :email,		presence: true, length: { maximum: 60 }, uniqueness: { case_sensitive: false },
 						format: { with: VALID_EMAIL_REGEX }
-	validates :firstname, uniqueness: {scope: :lastname, case_sensitive: false, message: "and Lastname have already been taken"}
 end
