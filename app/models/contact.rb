0bf4436935b87e7a3e7d385bf47d55ce1cca5485
zayrt <zelloufi.ilyas@gmail.com>
2015-10-14 22:19:47 +0200

    best validation

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,10 +1,9 @@
 class Contact < ActiveRecord::Base
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
 	VALID_USERNAME_REGEX = /\A[a-zA-Z]{1}[a-z0-9]+\z/
-	validates :firstname,	presence: true, length: { in: 3..30 }, uniqueness: { case_sensitive: false }, 
-						format: { with: VALID_USERNAME_REGEX }
-	validates :lastname,	presence: true, length: { in: 3..30 }, uniqueness: { case_sensitive: false },
-						format: { with: VALID_USERNAME_REGEX }
+	validates :firstname,	presence: true, length: { in: 3..30 }, format: { with: VALID_USERNAME_REGEX }
+	validates :lastname,	presence: true, length: { in: 3..30 }, format: { with: VALID_USERNAME_REGEX }
 	validates :email,		presence: true, length: { maximum: 60 }, uniqueness: { case_sensitive: false },
 						format: { with: VALID_EMAIL_REGEX }
+	validates :lastname, uniqueness: {scope: :firstname}
 end
